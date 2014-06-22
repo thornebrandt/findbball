70424f54e04f22679ae91afa527c4535a1ab2d30
Thorne Brandt <thornebrandt@gmail.com>
2014-06-21 21:38:26 -0500

    updated asset path for css

--- static/ref/deployment.sh
+++ static/ref/deployment.sh
@@ -1,12 +1,13 @@
 # directions for deployment
-cd findbball/rails
+cd findbball_rails
 
 export GEM_HOME=$PWD/gems
 export RUBYLIB=$PWD/lib
 export PATH=$PWD/bin:$PATH
 gem install bundle
 cd findbball
-../bin/
+rake db:migrate RAILS_ENV=production
+../bin/restart
 
 
 
