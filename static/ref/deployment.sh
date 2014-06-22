e2f865d09d2e391776e4330641ff911370349039
Thorne Brandt <thornebrandt@gmail.com>
2014-06-21 21:18:56 -0500

    deplyoment directions, changed reviews_container to members

---
+++ static/ref/deployment.sh
@@ -0,0 +1,14 @@
+# directions for deployment
+cd findbball/rails
+
+export GEM_HOME=$PWD/gems
+export RUBYLIB=$PWD/lib
+export PATH=$PWD/bin:$PATH
+gem install bundle
+cd findbball
+../bin/
+
+
+
+
+
