53a853ba2489e05947ddd1430c55a8535bd5a5ea
Max Silbiger <max.silbiger@gmail.com>
2014-07-06 22:06:27 -0400

    Court attr length validations

--- spec/models/court_spec.rb
+++ spec/models/court_spec.rb
@@ -3,11 +3,13 @@ require 'spec_helper'
 describe Court do
   
   let(:member) { FactoryGirl.create(:member) }
-  before { @court = member.courts.build(name: "Lorem ipsum", address: "1408 Lorem Rd", member: @member) }
+  before { @court = member.courts.build(name: "Courtem ipsum", address: "1408 Parkway Rd", member: @member) }
   
   subject { @court }
   
   it { should respond_to(:name) }
+  it { should respond_to(:address) }
+  it { should respond_to(:member) }
   it { should respond_to(:member_id) }
   its(:member) { should eq member }
   
@@ -15,6 +17,26 @@ describe Court do
   
   describe "when member_id is not present" do
     before { @court.member_id = nil }
+    it { should_not be_valid }
+  end
+  
+  describe "with blank name" do
+    before { @court.name = nil}
+    it { should_not be_valid }
+  end
+  
+  describe "with name that is too long" do
+    before { @court.name = "a" * 71 }
+    it { should_not be_valid }
+  end
+ 
+  describe "with blank address" do
+    before { @court.address = nil}
+    it { should_not be_valid }
+  end
+  
+  describe "with address that is too long" do
+    before { @court.address = "a" * 96 }
     it { should_not be_valid }
   end
     
