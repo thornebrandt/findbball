bab0038a396a580b355b11d84842d0f00a2bd036
Max Silbiger <max.silbiger@gmail.com>
2014-07-14 17:06:45 -0400

    Fixes image paths

--- spec/controllers/courts_controller_spec.rb
+++ spec/controllers/courts_controller_spec.rb
@@ -3,6 +3,13 @@ require 'spec_helper'
 describe CourtsController do
 
   integrate_views
+  
+  describe "GET 'index'" do
+    it "should be successful" do
+      get :index
+      response.should be_success
+    end
+  end
 
   describe "GET 'edit'" do
 
