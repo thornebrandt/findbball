24e167ef5ff42bc53d6be824492e8f203c5f2dfe
Max Silbiger <max.silbiger@gmail.com>
2014-07-17 21:45:16 -0400

    Signin for court specs, court attributes

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,3 +1,5 @@
+include ApplicationHelper
+
 def full_title(page_title)
 	base_title = "Findbball"
 	if page_title.empty?
@@ -5,4 +7,13 @@ def full_title(page_title)
 	else
 		"#{base_title} | #{page_title}"
 	end
+end
+
+def sign_in(member)
+  visit signin_path
+  fill_in "Email",    with: member.email
+  fill_in "Password", with: member.password
+  click_button "Sign in"
+  # Sign in when not using Capybara as well.
+  cookies[:remember_token] = member.remember_token
 end
