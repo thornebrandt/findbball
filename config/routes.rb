6d3115b47417d52d568fe03d5c2b72b077904588
Thorne Brandt <thornebrandt@gmail.com>
2014-07-19 23:23:48 -0500

    authentication pages passing

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,6 @@ Findbball::Application.routes.draw do
   match '/signout', to: 'sessions#destroy',   via: 'delete'
   match '/test/:id', to: 'members#show_test', via: 'get'
 
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -4,22 +4,21 @@ describe "AuthenticationPages" do
   describe "Authentication" do
   	subject { page }
 
-  	describe "signin" do
-  		before { visit signin_path }
-  		it { should have_content('Sign in') }
-  		it { should have_title('Sign in') }
+  	describe "Login" do
+  		before { visit home_path }
+  		it { should have_content('Login') }
 
   		describe "with valid information" do
-  			let(:member) { FactoryGirl.create(:member) } 
+  			let(:member) { FactoryGirl.create(:member) }
   			before do
   				fill_in "Email", with: member.email.upcase
   				fill_in "Password", with: member.password
   				click_button "Sign in"
   			end
 
-  			it { should have_selector('div#signed_in') }
-  			it { should have_link('Sign out', href: signout_path) }
-  			it { should_not have_link('Sign in', href: signin_path) }
+  			it { should have_link('Logout', href: signout_path) }
+            it { should have_selector('#logged_in') }
+  			it { should_not have_link('Login') }
   		end
 
   		describe "with invalid information" do
@@ -28,8 +27,8 @@ describe "AuthenticationPages" do
   		end
 
   		describe "after visiting another page" do
-  			before { click_link "Home" }
-  			it { should_not have_seletor('div.alert.alert.error') }
+  			before { click_link "logo" }
+  			it { should_not have_selector('div.alert.alert.error') }
   		end
   	end
   end
