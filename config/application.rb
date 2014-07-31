8ee9d0582cdc87e426f7f6691014648db1f7c64f
Thorne Brandt <thornebrandt@gmail.com>
2014-07-31 18:42:22 -0500

    compiled for production for presentation

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,6 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 require 'base64'
-require 'StringIO'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
