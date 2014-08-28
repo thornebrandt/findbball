4d3839bf86dd117e59679b6dff2f600ac19e3799
Thorne Brandt <thornebrandt@gmail.com>
2014-08-28 18:45:16 -0500

    name change

--- static/ref/deployment.sh
+++ static/ref/deployment.sh
@@ -1,14 +1,15 @@
-cd webapps/findbball
+cd webapps/findbball_rails/findbball
 git pull
 
 # directions for deployment on webfaction
-cd findbball_rails
+cd webapps/findbball_rails
 
 export GEM_HOME=$PWD/gems
 export RUBYLIB=$PWD/lib
 export PATH=$PWD/bin:$PATH
 gem install bundle
 cd findbball
+bundle install
 rake db:migrate RAILS_ENV=production
 ../bin/restart
 
