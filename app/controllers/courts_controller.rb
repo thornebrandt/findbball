68f404c812053af1a5458a8f0d88595a3d226471
Thorne Brandt <thornebrandt@gmail.com>
2014-09-10 03:39:32 -0500

    cleaned up map javascript, gets distances for found courts

--- app/controllers/courts_controller.rb
+++ app/controllers/courts_controller.rb
@@ -11,14 +11,15 @@ class CourtsController < ApplicationController
             @court = Court.find(params[:id])
             @court_photo = CourtPhoto.new if signed_in?
             @review = Review.new if signed_in?
-            @showMap = true;
+            @showMap = true
             @mapEl = "court_map"
-
             @shown_reviews = @court.reviews.last(4)
             @hidden_reviews = @court.reviews - @shown_reviews
             @court_photo = CourtPhoto.new if signed_in?
             @court_video = CourtVideo.new if signed_in?
             gon.court_photos = @court.court_photos.last(12);
+            gon.lat = @court.lat
+            gon.lng = @court.lng
             @review = Review.new if signed_in?
         else
             flash[:error] = "Could not find that court"
@@ -28,13 +29,16 @@ class CourtsController < ApplicationController
     end
 
     def find_hoops
+        @showMap = true;
+        @mapEl = "find_hoops_canvas";
         @ip = request.remote_ip
         @origin = params[:by]
-        @miles = params[:within] || 999
-
+        @miles = params[:within] || 100
         if !@origin
             if geo = session[:geo_location]
                 gon.geo = geo
+                gon.lat = geo.lat
+                gon.lng = geo.lng
                 @origin = [geo.lat, geo.lng]
             else
                 @origin = [Rails.configuration.lat, Rails.configuration.lng]
@@ -55,6 +59,8 @@ class CourtsController < ApplicationController
 
     def edit
         @court = Court.find(params[:id])
+        gon.lat = @court.lat
+        gon.lng = @court.lng
     if current_user? @court.member
         @showMap = true;
         @mapEl = "edit_court_map"
@@ -85,10 +91,17 @@ class CourtsController < ApplicationController
     @mapEl = "add_court_map"
     if signed_in?
         @court = Court.new
-
         @review = Review.new
         @review.court_id = @court.id
         @review.member_id = current_user.id
+        if geo = session[:geo_location]
+            gon.geo = geo
+            gon.lat = geo.lat
+            gon.lng = geo.lng
+        else
+            gon.lat = Rails.configuration.lat
+            gon.lng = Rails.configuration.lng
+        end
     else
         flash[:warning] = "You are not logged in."
         redirect_to home_path
