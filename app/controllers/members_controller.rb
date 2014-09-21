09092750c10932b8a00943eb1965b3c1f2811b4e
Thorne Brandt <thornebrandt@gmail.com>
2014-09-21 16:32:31 -0500

    verification complete

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -118,6 +118,43 @@ class MembersController < ApplicationController
         end
 	end
 
+
+    def verify_email
+        #localhost:3000/verify_email?i=13&q=60d39f4b37803e2c569829bb64
+        @member = Member.find(params[:i])
+        if @member
+            @email_verification = params[:q]
+            if @email_verification == @member.verification.to_s
+                if @member.update(registered: true)
+                    flash[:notice] = "Thank you for registering your email, #{@member.name}!"
+                    if signed_in?
+                        if current_user == @member
+                            redirect_to @member
+                        else
+                            sign_out
+                            sign_in @member
+                            redirect_to @member
+                        end
+                    else
+                        sign_in @member
+                        redirect_to @member
+                    end
+                end
+            else
+                flash[:error] = "Invalid email verification."
+                if @signed_in
+                    redirect_to current_user
+                else
+                    redirect_to home_path
+                end
+            end
+        else
+            flash[:error] = "Invalid email verification."
+            redirect_to home_path
+        end
+    end
+
+
 	private
 
         def split_base64(uri_str)
