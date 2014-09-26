b22c11ccd6e34cec9b5ced7453c60c1d458b7f20
Thorne Brandt <thornebrandt@gmail.com>
2014-09-26 02:20:46 -0500

    merge3

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,8 @@
 class StaticPagesController < ApplicationController
     def home
     	@hero = true
+        logger.debug "This is from debug --- ?"
+        logger.info "This is from info ---???"
     end
 
     def splash
