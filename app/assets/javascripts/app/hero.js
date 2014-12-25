34f3afd2d116c32bcc4fe06f6ebe9ffd2a4c160e
Thorne Brandt <thornebrandt@gmail.com>
2014-12-24 20:42:21 -0600

    links from hero space working

--- app/assets/javascripts/app/hero.js
+++ app/assets/javascripts/app/hero.js
@@ -1,4 +1,5 @@
 fbb.hero = function(){
+    fbb.currentSlide = false;
     var hero = $("#hero");
     var heroOptions = {
         cssTransitions:false,
@@ -6,11 +7,31 @@ fbb.hero = function(){
         cycleOptions: {
             fx:'scrollHorz',
             speed: 800,
-            timeout: 7000
+            timeout: 100,
+            after: function(last,current){
+                var heroContainer = $("#heroContainer");
+                heroContainer.removeClass("clickable");
+                fbb.currentSlide = current;
+                var heroLink = $(fbb.currentSlide).attr("data-href");
+                if(heroLink){
+                    heroContainer.addClass("clickable");
+                }
+            }
+
         }
     };
     var setupDomEvents = function(){
         hero.maximage(heroOptions);
+        $("#heroContainer").click(function(){
+            if(fbb.currentSlide){
+                var heroLink = $(fbb.currentSlide).attr("data-href");
+                if(heroLink){
+                    window.location = heroLink;
+                }
+            }
+        });
     };
+
+
     setupDomEvents();
 }
